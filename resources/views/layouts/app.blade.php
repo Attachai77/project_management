6c330e3547e1c897da310c50aebeca647ce09f02
Attachai (Wick) <attachai_sao@true-e-logistics.com>
2020-03-28 18:50:41 +0700

    change title

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'ระบบจัดการโครงการ') }}</title>
+    <title>ระบบบริหารจัดการโครงการ</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
